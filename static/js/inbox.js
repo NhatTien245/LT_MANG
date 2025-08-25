1c56dbf3b5a39fb3bfac44dba99b4bde005d148b
thaitoan02 <118003983+thaitoan02@users.noreply.github.com>
2025-08-25 19:15:33 +0700

    Add inbox.js. ẩn hiện các form

---
+++ static/js/inbox.js
@@ -0,0 +1,22 @@
+
+///__________Ẩn hiện các form__________///
+// ---- Ẩn hiện sidebar ---- //
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+});
+
+// Kết nối đến WebSocket server
+const socket = io();
+
+
+function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+}
